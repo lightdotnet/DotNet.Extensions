2610a6e62b16be6375b2f831d3fc33feb151f21d
minhvu <84697874+5-fire@users.noreply.github.com>
2025-07-11 17:15:33 +0700

    add EventBus and update workflows

---
+++ samples/EventBusConsumer/Common/ColorChangedIntegrationEvent.cs
@@ -0,0 +1,15 @@
+﻿using Light.EventBus.Events;
+
+namespace EventBusConsumer.Common
+{
+    [BindingName("color-value-changed")]
+    //[MessageUrn("color-value-changed")]
+    public record ColorChangedIntegrationEvent : EventBase
+    {
+        public string OldColor { get; set; } = null!;
+
+        public string NewColor { get; set; } = null!;
+
+        public DateTime ChangeOn { get; set; }
+    }
+}
---
+++ samples/EventBusConsumer/Common/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Light.AspNetCore.Modularity;
+using MassTransit;
+
+namespace EventBusConsumer.Common
+{
+    public class SampleModuleConsumers : ModuleConsumer
+    {
+        public override void AddConsumers(IBusRegistrationConfigurator configurator)
+        {
+            configurator.AddConsumer<ColorChangedConsumer, ColorChangedConsumerDefinition>();
+
+            Console.WriteLine($"Module {GetType().Name} injected");
+        }
+    }
+}
